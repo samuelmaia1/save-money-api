c2d01b3040c5125ff880e83e6e53a2cf64233bfa
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2025-02-19 21:44:47 -0300

    Subindo pro github

---
+++ src/main/java/com/samuelmaia1_github/SaveMoney/SaveMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.samuelmaia1_github.SaveMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveMoneyApplication.class, args);
+	}
+
+}
