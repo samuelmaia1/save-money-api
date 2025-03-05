84e90e98f6cc3cf3ba38e80dbb88518e827ceb0a
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2025-03-05 18:17:53 -0300

    Configuração de database e mapeamento de entidades e heranças

--- src/main/java/com/samuelmaia1_github/SaveMoneyAPI/models/Transaction.java
+++ src/main/java/com/samuelmaia1_github/SaveMoneyAPI/models/Transaction.java
@@ -1,4 +1,27 @@
 package com.samuelmaia1_github.SaveMoneyAPI.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Table(name = "transactions")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private Double amount;
+
+    private Boolean isRecurring;
+
+    private Date date;
+
+    private String description;
 }
